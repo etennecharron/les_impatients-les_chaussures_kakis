6f2e8ffa792a961992144e4d2b3039bdfccce11d
William Morel <morelwilliam2602@gmail.com>
2023-10-29 17:54:21 -0400

    equipe_erreur

--- script/script_erreur404.js
+++ script/script_erreur404.js
@@ -1,4 +1,12 @@
-gsap.timeline()
-.from('.drawanim', { opacity:100 })
-.from('.element.no2', { x: '400%' })
-.from('.element.no3', { x: '400%' }, '<0.25');
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline({paused: true});
+
+tl.to(".gsap_contenus", { x: 500, duration: 2 })
+	.to(".mask",{ scaleX: 0},"<")
+	.to(".campagne__contenus_gsap",{scale: 1, duration: 0.3}, 0.5)
+	.to(".text1",{ autoAlpha: 1, scale: 1}, "-=0.2")
+
+	.add(sequence2,"+=0.1").timeScale(0.8)
+
+	tl.seek(1.2).play()
