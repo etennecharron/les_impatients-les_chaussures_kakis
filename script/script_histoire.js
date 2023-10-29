1ed9406119977038c48ee79575827dbbe2bece9f
BADMIKQc <93718641+BADMIKQc@users.noreply.github.com>
2023-10-29 17:04:07 -0400

    scolltriger

---
+++ script/script_histoire.js
@@ -0,0 +1,77 @@
+
+
+gsap.to('.histoire__1992', 
+          
+{ 
+opacity:1,
+scrollTrigger:
+ 
+{
+  trigger: '.histoire__1992',
+  
+  scrub: 0.5,
+   start: 'top 70%',
+   end: 'bottom 25%',
+   }, 
+}
+
+
+         
+);
+
+gsap.to('.histoire__1999', 
+          
+{ 
+opacity:1,
+scrollTrigger:
+ 
+{
+  trigger: '.histoire__1999',
+  
+  scrub: 0.5,
+   start: 'top 70%',
+   end: 'bottom 25%',
+   }, 
+}
+
+
+         
+);
+
+gsap.to('.histoire__2009', 
+          
+{ 
+opacity:1,
+scrollTrigger:
+ 
+{
+  trigger: '.histoire__2009',
+  
+  scrub: 0.5,
+   start: 'top 70%',
+   end: 'bottom 25%',
+   }, 
+}
+
+
+         
+);
+
+gsap.to('.histoire__2011', 
+          
+{ 
+opacity:1,
+scrollTrigger:
+ 
+{
+  trigger: '.histoire__2011',
+  
+  scrub: 0.5,
+   start: 'top 70%',
+   end: 'bottom 25%',
+   }, 
+}
+
+
+         
+);
