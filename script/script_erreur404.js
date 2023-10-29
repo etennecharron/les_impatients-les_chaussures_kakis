7820de29ec72b59992b1bc2aea0d54bed65ccefd
William Morel <morelwilliam2602@gmail.com>
2023-10-29 18:25:00 -0400

    erreur 404

--- script/script_erreur404.js
+++ script/script_erreur404.js
@@ -1,12 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const tl = gsap.timeline({paused: true});
-
-tl.to(".gsap_contenus", { x: 500, duration: 2 })
-	.to(".mask",{ scaleX: 0},"<")
-	.to(".campagne__contenus_gsap",{scale: 1, duration: 0.3}, 0.5)
-	.to(".text1",{ autoAlpha: 1, scale: 1}, "-=0.2")
-
-	.add(sequence2,"+=0.1").timeScale(0.8)
-
-	tl.seek(1.2).play()
+gsap
+  .timeline()
+  .from(".drawanim", { x:"400%" },1)
+  .from(".h2_page_draw", { x:"400%" })
+  .from(".info_draw", { x:"400%"} )
+  .from(".use_loupe_draw", { x:"400%"} )
+  .from(".signaler_draw", { x:"400%"} )
+  .from(".search_draw", { x:"400%"} );
