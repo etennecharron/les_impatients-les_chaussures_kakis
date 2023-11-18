68b3c6f0b7f0bcb42fbe164c59a3d28f83e15acf
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2023-11-18 11:28:44 -0500

    bootstrap

---
+++ wordpress/wordpress/wp-content/themes/theme-de-base/home.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Modèle générique au cas où Wordpress ne trouve pas un modèle
+ * À utiliser comme fallback seulement.
+ */
+
+get_header(); // Affiche header.php
+if ( have_posts() ) : 
+	// Si oui, bouclons au travers pour tous les afficher
+	while ( have_posts() ) : the_post(); 
+?>
+
+
+
+
+
+<?php
+
+endwhile; wp_reset_postdata(); // Fermeture de la boucle
+
+else : // Si aucune page n'a été trouvée
+	get_template_part( 'partials/404' ); // Affiche partials/404.php
+endif;
+
+
+get_footer(); // Affiche footer.php 
+?>
