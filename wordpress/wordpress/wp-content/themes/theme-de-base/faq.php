366014505eae8882e44d8972f1f39e0dd3caca80
William Morel <morelwilliam2602@gmail.com>
2023-11-24 21:48:47 -0500

    php youpi

--- wordpress/wordpress/wp-content/themes/theme-de-base/faq.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/faq.php
@@ -1,31 +1,101 @@
-<?php 
+<?php
 /**
- * 	Template Name: Foires aux questions
- * 	Identique à page
+ * 	Template Name: FAQ
+ *  Template Post Type: post, page, groupe
+ */
+?>
+
+<?php
+/**
+ * Modèle permettant d'afficher un article (Post).
  */
 
 get_header(); // Affiche header.php
 
-if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ? 
-	// Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
-	while ( have_posts() ) : the_post(); 
+
+
 ?>
+ <section class="services">
+        <div class="services__wrapup">
+            <div class="services__wrapup__titre">
+                <h2>FAQ</h2>
+            </div>
+            <div class="services__wrapup__contenus">
+                <!-- utiliser le drop down bootstrap-->
+                <div class="services__wrapup__contenus__wrapupDropdown">
+                    <div class="services__wrapup__contenus__wrapupDropdown__titre">
+                        <h2>FAQ</h2>
+                    </div>
+                    <div>
+                        <div class="services__wrapup__contenus__wrapupDropdown--sourligner__atelier dropdown">
+                            <button class="btn dropdown-toggle" type="button" id="dropdownAtelier"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                <?php the_content(); ?>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownAtelier">
+                                <p>
+                                <?php the_content(); ?>
+                                </p>
+                            </div>
 
-	<article>
-		<?php if (!is_front_page()) : // Si nous ne sommes PAS sur la page d'accueil ?>
-			<h2>
-				<?php the_title(); // Titre de la page ?>
-			</h2>
-		<?php endif; ?>
-		
-		<?php the_content(); // Contenu principal de la page ?>
-	</article>
+                        </div>
+
+
+                        <div class="services__wrapup__contenus__wrapupDropdown--sourligner__collection dropdown">
+                            <button class="btn dropdown-toggle" type="button" id="dropdownCollection"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                <?php the_content(); ?>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownCollection">
+                                <p>
+                                <?php the_content(); ?>
+                                </p>
+                                <button>Voir la collection</button>
+                            </div>
+
+                        </div>
+
+
+                        <div class="services__wrapup__contenus__wrapupDropdown--sourligner__evenement dropdown">
+                            <button class="btn dropdown-toggle" type="button" id="dropdownEvenement"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Quels sont les évènements ?
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownEvenement">
+                                <p>
+                                <?php the_content(); ?>
+                                </p>
+                                <button>Voir les évenement</button>
+                            </div>
+
+                        </div>
+
+
+                        <div class="services__wrapup__contenus__wrapupDropdown--sourligner__evenement dropdown">
+                            <button class="btn dropdown-toggle" type="button" id="dropdownEvenement"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Quels sont les ateliers ?
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownEvenement">
+                                <p>
+                                <?php the_content(); ?>
+                                </p>
+                                <button>Voir les ateliers</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php endwhile; // Fermeture de la boucle
+		
+	get_template_part( 'partials/comments' ); // Affiche partials/comments.php
 
-else : // Si aucune page n'a été trouvée
-	get_template_part( 'partials/404' ); // Affiche partials/404.php
-endif;
+	else : // Si aucun article n'a été trouvée
+		get_template_part( 'partials/404' ); // Affiche partials/404.php
+	endif;
 
-get_sidebar(); // Affiche le contenu de sidebar.php
-get_footer(); // Affiche footer.php 
+	get_footer(); // Affiche footer.php 
 ?>
