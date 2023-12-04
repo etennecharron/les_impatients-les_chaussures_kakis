05dad8033e6ceef83d4e40d9ea648674e92d3cf1
William Morel <93773413+MorelWilliam@users.noreply.github.com>
2023-12-04 12:01:32 -0500

    404

--- wordpress/wordpress/wp-content/themes/theme-de-base/partials/404.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/partials/404.php
@@ -1,4 +1,9 @@
-<section>
-  <h2>404</h2>
-	<img src="https://media.giphy.com/media/3HLMNi9U0yb4CDldVO/giphy.gif" alt="404">
-</section>
+<div class="campagne__wrapup">
+            <img src="assets/images/Group 8.svg" alt="" class="drawanim">
+            <div class="campagne__wrapup__contenus">
+                <h2 class="h2_page_draw">Cette page n'est pas dessiné</h2>
+                <p class="info_draw">Pour rechercher une information :</p>
+                <p class="use_loupe_draw">Utiliser la loupe situé au dessous</p>
+                <div class="bar_recherche search_draw">Rechercher <i class="w-25 h-25 bi bi-search"></i></div>
+            </div>
+        </div>
