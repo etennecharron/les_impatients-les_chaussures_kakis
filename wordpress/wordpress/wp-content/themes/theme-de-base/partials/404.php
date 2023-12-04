625ad56452dc0735aaa7faafff3060f5f59d3868
William Morel <93773413+MorelWilliam@users.noreply.github.com>
2023-12-04 12:47:37 -0500

    etienne pull

--- wordpress/wordpress/wp-content/themes/theme-de-base/partials/404.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/partials/404.php
@@ -1,5 +1,6 @@
-<div class="campagne__wrapup">
-            <img src="assets/images/Group 8.svg" alt="" class="drawanim">
+<section class="campagne">
+        <div class="campagne__wrapup">
+        <img src="<?php bloginfo('template_url'); ?>/images/Group 8.svg" alt="" class="drawanim">
             <div class="campagne__wrapup__contenus">
                 <h2 class="h2_page_draw">Cette page n'est pas dessiné</h2>
                 <p class="info_draw">Pour rechercher une information :</p>
@@ -7,3 +8,4 @@
                 <div class="bar_recherche search_draw">Rechercher <i class="w-25 h-25 bi bi-search"></i></div>
             </div>
         </div>
+    </section>
