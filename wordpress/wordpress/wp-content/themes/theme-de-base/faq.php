c94171dd619564d4937bd0de12d873081e8810f5
William Morel <93773413+MorelWilliam@users.noreply.github.com>
2023-12-04 13:12:04 -0500

    J'ai fait certaine page Youpi

--- wordpress/wordpress/wp-content/themes/theme-de-base/faq.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/faq.php
@@ -1,21 +1,13 @@
-<?php
+<?php 
 /**
  * 	Template Name: FAQ
- *  Template Post Type: post, page, groupe
- */
-?>
-
-<?php
-/**
- * Modèle permettant d'afficher un article (Post).
+ *  Template Post Type: faq
+ * 	Identique à page
  */
 
 get_header(); // Affiche header.php
-
-
-
 ?>
- <section class="services">
+		    <section class="services">
         <div class="services__wrapup">
             <div class="services__wrapup__titre">
                 <h2>FAQ</h2>
@@ -30,11 +22,14 @@ get_header(); // Affiche header.php
                         <div class="services__wrapup__contenus__wrapupDropdown--sourligner__atelier dropdown">
                             <button class="btn dropdown-toggle" type="button" id="dropdownAtelier"
                                 data-bs-toggle="dropdown" aria-expanded="false">
-                                <?php the_content(); ?>
+                                Pourquoi est-ce que les Impatients existe-t-il ?
                             </button>
                             <div class="dropdown-menu" aria-labelledby="dropdownAtelier">
                                 <p>
-                                <?php the_content(); ?>
+                                    Les Impatients viennent en aide aux personnes ayant des problèmes de santé mentale
+                                    par le biais de l’expression artistique en offrant des ateliers de création qui
+                                    favorisent les échanges avec la communauté par la diffusion des réalisations
+                                    produites en atelier.
                                 </p>
                             </div>
 
@@ -44,11 +39,14 @@ get_header(); // Affiche header.php
                         <div class="services__wrapup__contenus__wrapupDropdown--sourligner__collection dropdown">
                             <button class="btn dropdown-toggle" type="button" id="dropdownCollection"
                                 data-bs-toggle="dropdown" aria-expanded="false">
-                                <?php the_content(); ?>
+                                Qu'est-ce qu'est la Collection ?
                             </button>
                             <div class="dropdown-menu" aria-labelledby="dropdownCollection">
                                 <p>
-                                <?php the_content(); ?>
+                                    Depuis le début de nos activités, nous avons conservé une grande partie des œuvres
+                                    produites par nos participants et participantes, ce qui constitue aujourd’hui un
+                                    patrimoine riche de 15 000 œuvres d’art. Il s’agit là d’une collection d’une grande
+                                    originalité, pionnière au Canada dans son champ de collectionnement.
                                 </p>
                                 <button>Voir la collection</button>
                             </div>
@@ -63,7 +61,9 @@ get_header(); // Affiche header.php
                             </button>
                             <div class="dropdown-menu" aria-labelledby="dropdownEvenement">
                                 <p>
-                                <?php the_content(); ?>
+                                    Nous disposons d’un espace galerie grâce auquel nous faisons connaître à un large
+                                    public les œuvres produites dans nos ateliers. À cela s’ajoutent d’autres activités
+                                    de diffusion régulièrement mises de l’avant à l’extérieur de notre centre.
                                 </p>
                                 <button>Voir les évenement</button>
                             </div>
@@ -78,7 +78,12 @@ get_header(); // Affiche header.php
                             </button>
                             <div class="dropdown-menu" aria-labelledby="dropdownEvenement">
                                 <p>
-                                <?php the_content(); ?>
+                                    Notre organisme est à même de transmettre son expertise aux centres et aux groupes
+                                    travaillant avec une clientèle en santé mentale. Plusieurs lieux d’ateliers ont été
+                                    ouverts au Québec en collaboration avec des établissements du milieu de la santé et
+                                    avec des lieux de diffusion impliqués dans leur communauté. Plusieurs ont suivi.
+                                    Aujourd’hui, nous avons 21 points de service dans 13 villes au Québec qui
+                                    accueillent 900 personnes par semaine.
                                 </p>
                                 <button>Voir les ateliers</button>
                             </div>
@@ -89,13 +94,9 @@ get_header(); // Affiche header.php
             </div>
         </div>
     </section>
-<?php endwhile; // Fermeture de la boucle
-		
-	get_template_part( 'partials/comments' ); // Affiche partials/comments.php
 
-	else : // Si aucun article n'a été trouvée
-		get_template_part( 'partials/404' ); // Affiche partials/404.php
-	endif;
+<?php 
 
-	get_footer(); // Affiche footer.php 
+get_sidebar(); // Affiche le contenu de sidebar.php
+get_footer(); // Affiche footer.php 
 ?>
