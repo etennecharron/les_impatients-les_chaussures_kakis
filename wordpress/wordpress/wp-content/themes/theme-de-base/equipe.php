cc74d4bbd94cdf5855b46ae9593d86a43c3b6c1d
William Morel <morelwilliam2602@gmail.com>
2023-12-14 03:21:34 -0500

    equipe

--- wordpress/wordpress/wp-content/themes/theme-de-base/equipe.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/equipe.php
@@ -20,104 +20,78 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
     <div class="services__wrapup__titre">
                 <h2>Notre équipe</h2>
             </div>
-        <div class="row justify-content-center">
-    
-        
-            <div class="col-lg-12 col-md-12 col-sm-12 justify-content-center">
-    
-            <?php
-      $role = new WP_Query('post_type=titre_equipe');
-      while ($role->have_posts()) : $role->the_post(); 
-    ?>
-    
-              <h2 class="equipe-role"><?php the_field('titre_role1')?></h2>
-    
-              <?php   endwhile;
-      wp_reset_postdata();
-    ?>  
-              
-                <div class="card-group equipe justify-content-center">
-                                    <?php
-      $projects = new WP_Query('post_type=equipe');
-      while ($projects->have_posts()) : $projects->the_post(); 
-    ?>
-                    <div data-bs-toggle="modal" data-bs-target="#exampleModal" class="card">
-                      <img class="card-img-top img-fluid" src=<?php the_post_thumbnail_url() ?> alt="Card image cap">
-                      <div class="card-body">
-                        <h3 class="card-title"><?php the_field('nom_de_lemploye')?></h5>
-                        <h5 class="card-text"><?php the_field('poste_de_lemploye')?></h5>
-                        <h5 class="card-text"><small><?php the_field('courriel')?></small></h5>
-                      </div>
+	
+    <div class="services__wrapup__contenus__wrapupDropdown__titre text-align-center justify-content-center">
+                        <h2 class="equipe">Administration</h2>
                     </div>
-                    <?php   endwhile;
-      wp_reset_postdata();
-    ?>
-            </div>
-        </div>
-            <div class="col-lg-12 col-md-12 col-sm-12">
-            <?php
-      $role = new WP_Query('post_type=titre_equipe');
-      while ($role->have_posts()) : $role->the_post(); 
-    ?>
-    
-              <h2 class="equipe-role"><?php the_field('titre_role2')?></h2>
-    
-              <?php   endwhile;
-      wp_reset_postdata();
-    ?>  
-                <div class="card-group equipe justify-content-center">
-                <?php
-      $integration = new WP_Query('post_type=equipe_accueil');
-      while ($integration->have_posts()) : $integration->the_post(); 
-    ?>
-                    <div data-bs-toggle="modal" data-bs-target="#exampleModal" class="card">
-                      <img class="card-img-top" src=<?php the_post_thumbnail_url() ?> alt="Card image cap">
-                      <div class="card-body">
-                        <h3 class="card-title"><?php the_field('nom_de_lemploye')?></h3>
-                        <h5 class="card-text"><?php the_field('poste_de_lemploye')?></h5>
-                        <h5 class="card-text"><small><?php the_field('courriel')?></small></h5>
-                      </div>
+
+		  
+			<div class="EquipeAdmin card-group equipe justify-content-center container">
+								<?php
+  $admin = new WP_Query('post_type=administration');
+  while ($admin->have_posts()) : $admin->the_post(); 
+?>
+				<div data-bs-toggle="modal" data-bs-target="#exampleModal" class="card">
+				  <img class="card-img-top img-fluid" src=<?php the_post_thumbnail_url() ?> alt="Card image cap">
+				  <div class="card-body">
+					<h3 class="card-title"><?php the_field('nom_de_lemploye')?></h5>
+					<h5 class="card-text"><?php the_field('poste_de_lemploye')?></h5>
+					<h5 class="card-text"><?php the_field('courriel')?></h5>
+				  </div>
+				</div>
+				<?php   endwhile;
+  wp_reset_postdata();
+?>
+		</div>
+	</div>
+    <div class="services__wrapup__contenus__wrapupDropdown__titre justify-content-center">
+                        <h2>Artistes et art-thérapeutes responsables d'ateliers</h2>
+                    </div>			
+                    
+                    <div class="EquipeArtiste card-group equipe justify-content-center container">
+			<?php
+  $artiste = new WP_Query('post_type=artiste');
+  while ($artiste->have_posts()) : $artiste->the_post(); 
+?>
+				<div data-bs-toggle="modal" data-bs-target="#exampleModal" class="card">
+				  <img class="card-img-top" src=<?php the_post_thumbnail_url() ?> alt="Card image cap">
+				  <div class="card-body">
+					<h3 class="card-title"><?php the_field('nom_de_lemploye')?></h3>
+				  </div>
+				</div>
+				<?php   endwhile;
+  wp_reset_postdata();
+?>
+
+		</div>
+	</div>
+    <div class="services__wrapup__contenus__wrapupDropdown__titre justify-content-center">
+                        <h2>Conseil d'administration</h2>
                     </div>
-                    <?php   endwhile;
-      wp_reset_postdata();
-    ?>
-    
-            </div>
-        </div>
-            <div class="col-lg-12 col-md-12 col-sm-12">
-            <?php
-      $role = new WP_Query('post_type=titre_equipe');
-      while ($role->have_posts()) : $role->the_post(); 
-    ?>
-    
-              <h2 class="equipe-role"><?php the_field('titre_role3')?></h2>
-    
-              <?php   endwhile;
-      wp_reset_postdata();
-    ?>  
-                <div class="card-group equipe justify-content-center">
-                <?php
-      $francisation = new WP_Query('post_type=equipe_francisation');
-      while ($francisation->have_posts()) : $francisation->the_post(); 
-    ?>
-                    <div data-bs-toggle="modal" data-bs-target="#exampleModal" class="card">
-                      <img class="card-img-top" src=<?php the_post_thumbnail_url() ?> alt="Card image cap">
-                      <div class="card-body">
-                        <h3 class="card-title"><?php the_field('nom_de_lemploye')?></h3>
-                        <h5 class="card-text"><?php the_field('poste_de_lemploye')?></h5>
-                        <h5 class="card-text"><small><?php the_field('courriel')?></small></h5>
-                      </div>
-                    </div>
-                    <?php   endwhile;
-      wp_reset_postdata();
-    ?>
-    
-                  </div>
-    </div>
-    </div>
-    </div>
-    
-    </section>
+			<div class="EquipeConseil card-group equipe justify-content-center container">
+			<?php
+  $conseil = new WP_Query('post_type=conseil-admin');
+  while ($conseil->have_posts()) : $conseil->the_post(); 
+?>
+				<div data-bs-toggle="modal" data-bs-target="#exampleModal" class="card">
+				  <img class="card-img-top" src=<?php the_post_thumbnail_url() ?> alt="Card image cap">
+				  <div class="card-body">
+					<h3 class="card-title"><?php the_field('nom_de_lemploye')?></h3>
+					<h5 class="card-text"><?php the_field('poste_de_lemploye')?></h5>
+				  </div>
+				</div>
+				<?php   endwhile;
+  wp_reset_postdata();
+?>
+
+			  </div>
+
+		</div>
+		
+	</div>
+</div>
+
+</section>
 		
 
 <?php endwhile; // Fermeture de la boucle
